9dcf579c5a5028d46b84f52b076aac3ee4581ba0
nemopss <alexey_gladilin@mail.ru>
2024-06-06 23:54:44 +0300

    Created CLI shell

--- cmd/do.go
+++ cmd/do.go
@@ -2,16 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
-// doCmd represents the do command
 var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
 	},
 }
 
