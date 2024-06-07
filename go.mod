f3fcef3f6d5eb008f78cf2afaeb25f85a0475a81
nemopss <alexey_gladilin@mail.ru>
2024-06-07 20:15:56 +0300

    Created a tasks bucket in db

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.8.0
 )
 
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/nemopss/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/nemopss/task/cmd"
+	"github.com/nemopss/task/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
